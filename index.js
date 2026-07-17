5dfa87c6ebf3b90f0cd56b13a18f5249dfc0da21
GabrielaLunardi <gabrielalunardi1997@gmail.com>
2026-07-16 22:16:17 -0300

    Classes de um jogo

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 console.log("Classes de um jogo");
+
+class Character {
+    constructor(name, age, type) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+
+    }
+    attack() {
+
+        let attackType = "";
+
+        if (this.type === "guerreiro") {
+            attackType = "espada";
+
+        } else if (this.type === "mago") {
+            attackType = "magia";
+
+        } else if (this.type === "monge") {
+            attackType = "artes marciais";
+
+        } else if (this.type === "ninja") {
+            attackType = "shuriken";
+        } else {
+            attackType = "ataque desconhecido";
+        }
+        
+        console.log(`O personagem ${this.name} que é um ${this.type} de ${this.age} anos, atacou usando ${attackType}.`);
+    }
+}
+
+let character1 = new Character("Aragorn", 30, "guerreiro");
+let character2 = new Character("Gandalf", 100, "mago");
+let character3 = new Character("Bruce Lee", 32, "monge");
+let character4 = new Character("Naruto", 20, "ninja");
+
+character1.attack();
+character2.attack();
+character3.attack();
+character4.attack();
